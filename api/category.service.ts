0ba2ea0aec7324379df1f5a8fcc59fc2b9cc9b33
Crhistian Ramirez <cramirez@four51.com>
2018-11-14 13:44:13 -0600

    Implement tree-shakeable providers introduced in angular v6 #4

--- api/category.service.ts
+++ api/category.service.ts
@@ -26,7 +26,9 @@ import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables'
 import { Configuration }                                     from '../configuration';
 
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class OcCategoryService {
 
     protected basePath = 'https://api.ordercloud.io/v1';
--- api/supplier.service.ts
+++ api/supplier.service.ts
@@ -22,7 +22,9 @@ import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables'
 import { Configuration }                                     from '../configuration';
 
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class OcSupplierService {
 
     protected basePath = 'https://api.ordercloud.io/v1';
